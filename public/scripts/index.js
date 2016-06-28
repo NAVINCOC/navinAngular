37028a6065328fa58492d2b6ef621a0820253777
Navin Bisht <api.integration@paytm.com>
2016-06-28 15:26:52 +0530

    final

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -9,10 +9,7 @@ module.exports = {
     res.render('login');
   },
   confmLogin: function(req, res) {
-<<<<<<< HEAD
-=======
   	var data={email:'er.ashsingla@gmail.com',password:'asdfgh@123'};
->>>>>>> 7ad980fecd68ce9581e951f5153f3348635c8dae
     var options = {
       url: 'http://127.0.0.1:2318/v1/login',
       method: 'POST',
@@ -20,11 +17,7 @@ module.exports = {
         key: 'NAVNIV',
         userid: '2318'
       },
-<<<<<<< HEAD
-      form: req.body
-=======
       form: data
->>>>>>> 7ad980fecd68ce9581e951f5153f3348635c8dae
     };
     
     request(options, function(err, response, body) {
@@ -32,11 +25,7 @@ module.exports = {
         res.status(200).send(body);
     });
   },
-<<<<<<< HEAD
-  register: function(req, res) {
-=======
   register: function(req, res) { console.log("123",req.body);
->>>>>>> 7ad980fecd68ce9581e951f5153f3348635c8dae
   	var options = {
       url: 'http://127.0.0.1:2318/v1/register',
       method: 'POST',
@@ -48,10 +37,6 @@ module.exports = {
     };
     
     request(options, function(err, response, body) {
-<<<<<<< HEAD
-        if (err) console.log('error    '.error, err)
-        res.status(200).send(body);
-=======
         if (err) {console.log('error    '.error, err);}
         /*else if(body[0] === '[')
         {*/
@@ -60,7 +45,6 @@ module.exports = {
 			res.status(200).send(body);
 		/*} */      
         
->>>>>>> 7ad980fecd68ce9581e951f5153f3348635c8dae
     });
   }
 };
